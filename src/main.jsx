555121936ca0635d437599058347c67da9459377
iRhyke <256hayato@gmail.com>
2026-03-14 20:18:08 +0900

    main.jsxにSpeedinsightsのコンポーネントを追加

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,6 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
-    <SpeedInsights
-      url="https://www.yourwebsite.com"
-      apiKey="YOUR_VERCEL_API_KEY"
-    /> 
+    <SpeedInsights/> 
   </>
 )
