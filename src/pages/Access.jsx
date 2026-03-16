91e3fff4ad61c808bfdc5298d475ddd888b7f22b
iRhyke <256hayato@gmail.com>
2026-03-16 12:24:55 +0900

    説明

--- src/pages/Access.jsx
+++ src/pages/Access.jsx
@@ -24,7 +24,7 @@ export default function Access() {
               className="rounded-2xl overflow-hidden shadow-lg h-[400px] lg:h-[500px]"
             >
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d407.80512466742584!2d136.9941608!3d35.145621!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600365728395c39b%3A0x146777364a0e3462!2sGOLF%20SALON%20GREEN!5e0!3m2!1sja!2sjp!4v1773630561865!5m2!1sja!2sjp"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9889.904629084584!2d136.98623856427417!3d35.14529111783757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600365728395c39b%3A0x146777364a0e3462!2sGOLF%20SALON%20GREEN!5e0!3m2!1sja!2sjp!4v1773631148514!5m2!1sja!2sjp"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
